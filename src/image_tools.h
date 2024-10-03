4c0773f7c9f196b46442a0bdccd5b1fbad361228
janvanderwatt <janvanderwatt@gmail.com>
2024-10-03 15:04:55 +1000

    Baseline.

---
+++ src/image_tools.h
@@ -0,0 +1,6 @@
+#ifndef _JVDW_IMAGE_TOOLS_H
+#define _JVDW_IMAGE_TOOLS_H
+
+#define COLOR565(red, green, blue) (((red & 0xF8) << 8) | ((green & 0xFC) << 3) | (blue >> 3))
+
+#endif  // #ifndef _JVDW_IMAGE_TOOLS_H
